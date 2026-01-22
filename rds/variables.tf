0e1ad7147545d46216fed32b204eaa6d9c99ce67
Matheusvavila <matheusvavilla@gmail.com>
2026-01-22 20:21:41 -0300

    ajuste engine postgres

--- rds/variables.tf
+++ rds/variables.tf
@@ -19,8 +19,8 @@ variable "engine" {
   description = "Engine do banco de dados MySQL, PostgreSQL, Oracle, SQLServer..."
   type        = string
   validation {
-    condition     = contains(["mysql", "postgresql"], var.engine)
-    error_message = "O valor da variável engine deve ser 'mysql' ou 'postgresql'."
+    condition     = contains(["mysql", "postgres"], var.engine)
+    error_message = "O valor da variável engine deve ser 'mysql' ou 'postgres'."
   }
 }
 variable "engine_version" {
