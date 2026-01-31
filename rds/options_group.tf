e91d9a64cebfaef964c29864a5fd036d7ee3b629
Matheusvavila <matheusvavilla@gmail.com>
2026-01-31 19:48:44 -0300

    ajuste no name do options group removendo } errado

--- rds/options_group.tf
+++ rds/options_group.tf
@@ -1,5 +1,5 @@
 resource "aws_db_option_group" "options_group" {
-  name                     = "${var.identifier}${var.engine}${replace(var.engine_version, ".", "")}}"
+  name                     = "${var.identifier}${var.engine}${replace(var.engine_version, ".", "")}"
   option_group_description = "Terraform Option Group"
   engine_name              = var.engine
   major_engine_version     = var.major_engine_version
