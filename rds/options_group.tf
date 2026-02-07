b635969dbc0d3f4240095faca35fd4fa6f9d92ce
Matheusvavila <matheusvavilla@gmail.com>
2026-02-07 18:19:57 -0300

    remove SERVER_AUDIT_FILE_ROTATE_SIZE

--- rds/options_group.tf
+++ rds/options_group.tf
@@ -15,10 +15,10 @@ resource "aws_db_option_group" "options_group" {
       name = "SERVER_AUDIT_FILE_ROTATIONS"
       value = "50"
     }
-    option_settings {
-      name = "SERVER_AUDIT_FILE_ROTATE_SIZE"
-      value = "2000000"
-    }
+ #   option_settings {
+ #     name = "SERVER_AUDIT_FILE_ROTATE_SIZE"
+ #     value = "2000000"
+ #   }
   }
   lifecycle {
     prevent_destroy = true
