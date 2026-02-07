f07f8348fb61c07372ce06e941ac63b51ab95376
Matheusvavila <matheusvavilla@gmail.com>
2026-02-07 16:35:25 -0300

    teste

--- rds/options_group.tf
+++ rds/options_group.tf
@@ -9,7 +9,7 @@ resource "aws_db_option_group" "options_group" {
 
     option_settings {
       name = "SERVER_AUDIT_EVENTS"
-      value = ["CONNECT, QUERY, QUERY_DDL, QUERY_DML, QUERY_DCL, QUERY_DML_NO_SELECT"]
+      value = "CONNECT, QUERY, QUERY_DDL, QUERY_DML, QUERY_DCL, QUERY_DML_NO_SELECT"
     }
     option_settings {
       name = "SERVER_AUDIT_FILE_ROTATIONS"
