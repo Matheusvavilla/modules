485cb6edc0dee563cef2e25f7b889c5e756f1df4
Matheusvavila <matheusvavilla@gmail.com>
2026-02-14 22:57:58 -0300

    tshoot

--- rds/rds.tf
+++ rds/rds.tf
@@ -4,9 +4,11 @@ resource "aws_db_instance" "mydbterraform" {
   engine                      = var.engine
   engine_version              = var.engine_version
   allow_major_version_upgrade = var.allow_major_version_upgrade
+/*
   blue_green_update {
     enabled = true
   }
+*/
 
   instance_class = var.instance_class
 
